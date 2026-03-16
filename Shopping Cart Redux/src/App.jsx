e23c10af90b9d0e43b04b73e4436476143411ec3
Himu2002 <hiimanshurawat@gmail.com>
2026-03-16 21:44:02 +0530

    chore: add remaining configuration and assets for Shopping Cart Redux

---
+++ Shopping Cart Redux/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Counter from './components/counter'
+import Product from './components/product'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter /> */}
+      <Product />
+    </div>
+  )
+}
+
+export default App
